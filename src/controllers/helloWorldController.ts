71e0e318ffea1177fbe69fcb4ee05724de782bbb
BiggerThings <nnsbiggerthings@gmail.com>
2025-11-03 16:40:54 +0200

    Logs debugging: Pt. 1

--- src/controllers/helloWorldController.ts
+++ src/controllers/helloWorldController.ts
@@ -1,5 +1,11 @@
 import { Request, Response } from 'express';
 
 export const helloWorld = (req: Request, res: Response) => {
-    res.status(200).send({message: 'Hello, World!'});
+    try {
+        res.status(200).send({message: 'Hello, World!'});
+        console.error('Hello World endpoint was called');
+    } catch (error) {
+        console.error('Error occurred:', error);
+        res.status(500).send({error: 'Internal Server Error'});
+    }
 };
